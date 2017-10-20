6d0f1cebc9c076f28b86b46384b3b7146194c864
Tarunyo <32845547+Tarunyo@users.noreply.github.com>
2017-10-20 10:57:37 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,22 @@
-def fibonacci(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
-
-for i in range(0, 10):
-    print(fibonacci(i))
+//fibonacci in turbo c++
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a=0, b=1, c=0, limit;
+	cout<<"Upto How many term ? ";
+	scanf("%d",&limit);
+	cout<<"Fabonacci Series : "<<a<<" "<<b<<" ";  // first two term
+	c=a+b;
+	limit=limit-2;   // decrease the limit by 2. since two numbers already printed
+	while(limit)
+	{
+		cout<<c<<" ";
+		a=b;
+		b=c;
+		c=a+b;
+		limit--;
+	}
+	getch();
+}
