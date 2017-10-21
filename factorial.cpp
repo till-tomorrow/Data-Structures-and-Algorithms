e0e1f27d100cc44bc80f581bd219cb8ed7b85d18
cathreya <cathreya98@gmail.com>
2017-10-21 13:17:47 +0530

    Factorial recursive C++ implementation

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+	if(n < 1){
+		return 1;
+	}
+	return n*fact(n-1);
+}
+
+int main(){
+	int n;
+	cout<<"Enter No.\n";
+	cin>>n;
+	cout<<fact(n)<<'\n';
+}
