1ec280fe737b1ff3e4f8ed3739217eb96d73708d
Kalskiman <Robin.Kalkowski@gmx.de>
2017-10-23 12:14:51 +0200

    Add files via upload

---
+++ BubbleSortWithC#.cs
@@ -0,0 +1,22 @@
+public void StartSorting()
+{
+        int[] arr = { 800, 11, 50, 771, 649, 770, 240, 9 }; //could be customized
+
+        int temp = 0;
+
+        for (int write = 0; write < arr.Length; write++) {
+            for (int sort = 0; sort < arr.Length - 1; sort++) {
+                if (arr[sort] > arr[sort + 1]) {
+                    temp = arr[sort + 1];
+                    arr[sort + 1] = arr[sort];
+                    arr[sort] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+            Console.Write(arr[i] + " ");
+
+Console.ReadKey();
+
+}
